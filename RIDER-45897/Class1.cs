ac4bf2b5e87680af149ed69014c70017487e38d1
Kyle Polansky <kylepolansky@gmail.com>
2020-07-20 23:21:36 -0500

    Sample repro of RIDER-45897

---
+++ RIDER-45897/Class1.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+namespace RIDER_45897
+{
+    public class Class1
+    {
+        private class Foo
+        {
+            public string Bar { get; set; }
+        }
+
+        public void Sample1()
+        {
+            // Correct according to .editorconfig
+            var foo = new Foo {
+                Bar = "Bar"
+            };
+
+            // RIDER 2020.2 EAP suggests this fix, which causes a build error
+            // var foo = new Foo
+            // {
+            //     Bar = "Bar"
+            // };
+        }
+
+        public void Sample2()
+        {
+            // Correct according to .editorconfig
+            Func<string> foo = () => {
+                return "bar";
+            };
+
+            // RIDER 2020.2 EAP suggests this fix, which causes a build error
+            // Func<string> foo = () =>
+            // {
+            //     return "bar";
+            // };
+        }
+    }
+}
